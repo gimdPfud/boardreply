a946c539fb1ffa8c2aa1b85554ef406a4ef6f32e
segong <sy981022@naver.com>
2025-03-17 14:31:44 +0900

    board entity 작성

---
+++ src/main/java/com/example/boardreply/entity/board.java
@@ -0,0 +1,32 @@
+package com.example.boardreply.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    private LocalDateTime regitime;
+
+}
