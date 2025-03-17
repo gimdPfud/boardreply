fde852d71c5b418696b78589eaa8eefb700bf483
segong <sy981022@naver.com>
2025-03-17 14:38:32 +0900

    BoardDTO 작성

---
+++ src/main/java/com/example/boardreply/dto/BoardDTO.java
@@ -0,0 +1,17 @@
+package com.example.boardreply.dto;
+
+import java.time.LocalDateTime;
+
+public class BoardDTO {
+
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    private LocalDateTime regitime;
+
+}
